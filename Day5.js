cbab1a83b06cfc21d4348a253df5126f4430c783
swathigadde <swathi.vs1126@gmail.com>
2026-02-18 22:43:47 -0600

    consolestatemnt

--- Day5.js
+++ Day5.js
@@ -1,6 +1,6 @@
 let day='sunday';
 if(day==='sunday' || day==='saturday'){
-    console.log("It's a weekend!");
+    console.log("It's a weekend! Happy holidays enjoy your days");
 }
 else{
     console.log("It's a weekday.");
